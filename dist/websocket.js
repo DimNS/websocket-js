ae947c0dfe9558f23fb8ff47c647929bb3e260ee
Дмитрий Щербаков <atomcms@ya.ru>
2019-04-11 11:32:15 +0500

    build Билд 1.2.0

--- dist/websocket.js
+++ dist/websocket.js
@@ -14,73 +14,97 @@ var webSocketPHP = {};
 /**
  * Инициализация
  *
- * @version 27.03.2019
+ * @version 11.04.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 webSocketPHP.init = function (options) {
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
+    // Проверка на наличие возможности WebSocket
     if (!window.WebSocket) {
         console.info('WebSocket not supported');
 
         return;
     }
 
-    if (typeof (options) === 'undefined') {
+    // Проверка на наличие объекта
+    if (typeof options === 'undefined') {
         options = {};
     }
 
+    // Настройки
     webSocketPHP._settings = $.extend({
         url            : null,
         sid            : null,
         attemptsCount  : 5,
         attemptsTimeout: 10,
+        debug          : false,
+        onConnect      : null,
+        onDisconnect   : null,
         onMessage      : function (message) {
             console.info(message);
         }
     }, options);
 
+    // Проверка наличия обязательного параметра url
     if (webSocketPHP._settings.url === null || webSocketPHP._settings.url === '') {
         console.error('WebSocket: Option "url" is empty');
 
         return;
     }
 
+    // Проверка наличия обязательного параметра sid
     if (webSocketPHP._settings.sid === null || webSocketPHP._settings.sid === '') {
         console.error('WebSocket: Option "sid" is empty');
 
         return;
     }
 
+    // Первая попытка соединения
     webSocketPHP._settings.attemptsNum = 1;
 
+    // Запустим установку соединения
     webSocketPHP._connect();
 };
 /**
  * Подключение к WebSocket серверу
  *
- * @version 27.03.2019
+ * @version 11.04.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 webSocketPHP._connect = function () {
     var ws = new WebSocket(webSocketPHP._settings.url + '/?sid=' + webSocketPHP._settings.sid);
 
     ws.onopen = function () {
-        console.info('WebSocket connection success');
+        if (webSocketPHP._settings.debug === true) {
+            console.info('WebSocket connection success');
+        }
 
         // При успешном подключении сбрасываем попытки подключений
         webSocketPHP._settings.attemptsNum = 0;
+
+        if (typeof webSocketPHP._settings.onConnect === 'function') {
+            webSocketPHP._settings.onConnect();
+        }
     };
 
     ws.onerror = function (error) {
-        console.error('WebSocket error', error);
+        if (webSocketPHP._settings.debug === true) {
+            console.error('WebSocket error', error);
+        }
     };
 
     ws.onclose = function (event) {
-        if (event.wasClean) {
-            console.info('WebSocket connection is closed');
-        } else {
-            console.error('WebSocket connection failure code: ' + event.code);
+        if (webSocketPHP._settings.debug === true) {
+            if (event.wasClean) {
+                console.info('WebSocket connection is closed');
+            } else {
+                console.error('WebSocket connection failure code: ' + event.code);
+            }
+        }
+
+        if (typeof webSocketPHP._settings.onDisconnect === 'function') {
+            webSocketPHP._settings.onDisconnect();
         }
 
         // Проверяем номер попытки
@@ -96,6 +120,8 @@ webSocketPHP._connect = function () {
     };
 
     ws.onmessage = function (result) {
-        webSocketPHP._settings.onMessage(JSON.parse(result.data));
+        if (typeof webSocketPHP._settings.onMessage === 'function') {
+            webSocketPHP._settings.onMessage(JSON.parse(result.data));
+        }
     };
 };
